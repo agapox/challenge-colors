7dce3ccffb8fa64afa4771708d2a2e67a2678981
jramirez <jose.ramirez@siigroup.cl>
2021-04-30 13:23:56 -0400

    feature: copy color to clipboard

--- src/app/components/color-card/color-card.component.ts
+++ src/app/components/color-card/color-card.component.ts
@@ -16,9 +16,34 @@ export class ColorCardComponent implements OnInit {
     pantone_value: ''
   };
 
+  copyMsg: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleColorSelected(ev: MouseEvent, color: string) {
+
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = color;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.showCopiedMsg();
+  }
+
+  showCopiedMsg() {
+    this.copyMsg = true;
+    setTimeout(() => {
+      this.copyMsg = false;
+    }, 1300);
   }
 
 }

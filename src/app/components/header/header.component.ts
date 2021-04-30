e387603cc039c5f486f02ee45b56a00dce782f4a
jramirez <jose.ramirez@siigroup.cl>
2021-04-30 16:03:52 -0400

    fix: header page navigation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewChecked {
 
-  title: string = 'Colores'
+  @Output() changePageEv = new EventEmitter();
+  @Input() totalPages: number = 0;
+  @Input() currentPage: number = 0;
+  title: string = 'Colores';
+  disabledPrevBtn: boolean = true;
+  disabledNextBtn: boolean = true;
 
-  constructor() { }
+  constructor(
+    private cdRef: ChangeDetectorRef
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  ngAfterViewChecked() {
+    this.disableBtns();
+    this.cdRef.detectChanges()
+  }
+
+  disableBtns() {
+    this.disabledPrevBtn = true;
+    this.disabledNextBtn = true;
+    if (this.currentPage !== 1) {
+      this.disabledPrevBtn = false;
+    } else if (this.currentPage < this.totalPages) {
+      this.disabledNextBtn = false;
+    }
+    console.log(this.currentPage)
+    console.log(this.totalPages)
+  }
+
+  handleChangePage(to: ('next' | 'prev')) {
+    this.disableBtns();
+    this.changePageEv.emit(to);
   }
 
 }
